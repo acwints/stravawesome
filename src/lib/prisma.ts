c0011c360a956aa9206ac482cbc02827a42986d2
Andrew Winter <acwints78@gmail.com>
2025-01-15 14:01:21 -0800

    fix: update prisma config for pgbouncer compatibility

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,21 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['error'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      },
+    },
+  })
 }
 
-const prisma = globalThis.prisma || new PrismaClient()
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma
